ac1ebac5d845cc1bd4e7947ae0da86c4a7c0f6be
emersion <contact@emersion.fr>
2019-01-07 22:51:11 +0100

    wip: job control

--- shell/process.c
+++ shell/process.c
@@ -4,6 +4,8 @@
 #include <sys/wait.h>
 #include "shell/process.h"
 
+#include <stdio.h>
+
 // TODO: don't use a global
 // TODO: use a linked list instead
 static struct mrsh_array running_processes = {0};
@@ -11,8 +13,6 @@ static struct mrsh_array running_processes = {0};
 void process_init(struct process *proc, pid_t pid) {
 	mrsh_array_add(&running_processes, proc);
 	proc->pid = pid;
-	proc->finished = false;
-	proc->stat = 0;
 }
 
 int process_poll(struct process *proc) {
@@ -45,9 +45,13 @@ void process_notify(pid_t pid, int stat) {
 	for (size_t i = 0; i < running_processes.len; ++i) {
 		struct process *proc = running_processes.data[i];
 		if (proc->pid == pid) {
-			proc->finished = true;
 			proc->stat = stat;
-			process_remove(proc);
+			if (WIFSTOPPED(stat)) {
+				proc->stopped = true;
+			} else if (WIFEXITED(stat)) {
+				proc->finished = true;
+				process_remove(proc);
+			}
 			break;
 		}
 	}
